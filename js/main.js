2b843825415d9f949958ac06c14d0b4e5e84b4ed
Javi Morales <javiermoralesfernandez@gmail.com>
2017-04-25 18:09:17 +0200

    added qr plugin

--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 function scan()
 {
     alert('tadaaaa');
-    cordova.plugins.barcodeScanner.scan(
-        function (result) {
-            if(!result.cancelled)
-            {
-                if(result.format == "QR_CODE")
-                {
-                    navigator.notification.prompt("Please enter name of data",  function(input){
-                        var name = input.input1;
-                        var value = result.text;
+    QRScanner.prepare(onDone); // show the prompt
 
-                        var data = localStorage.getItem("LocalData");
-                        console.log(data);
-                        data = JSON.parse(data);
-                        data[data.length] = [name, value];
-
-                        localStorage.setItem("LocalData", JSON.stringify(data));
-
-                        alert("Done");
-                    });
-                }
-            }
-        },
-        function (error) {
-            alert("Scanning failed: " + error);
+    function onDone(err, status){
+        if (err) {
+            // here we can handle errors and clean up any loose ends.
+            console.error(err);
         }
-    );
+        if (status.authorized) {
+            // W00t, you have camera access and the scanner is initialized.
+            // QRscanner.show() should feel very fast.
+        } else if (status.denied) {
+            // The video preview will remain black, and scanning is disabled. We can
+            // try to ask the user to change their mind, but we'll have to send them
+            // to their device settings with `QRScanner.openSettings()`.
+        } else {
+            // we didn't get permission, but we didn't get permanently denied. (On
+            // Android, a denial isn't permanent unless the user checks the "Don't
+            // ask again" box.) We can ask again at the next relevant opportunity.
+        }
+    }
+    QRScanner.scan(displayContents);
+
+    function displayContents(err, text){
+        if(err){
+            // an error occurred, or the scan was canceled (error code `6`)
+        } else {
+            // The scan completed, display the contents of the QR code:
+            alert(text);
+        }
+    }
+
+// Make the webview transparent so the video preview is visible behind it.
+    QRScanner.show();
+// Be sure to make any opaque HTML elements transparent here to avoid
+// covering the video.
+
 }
+
 
 function hideLayer(layer){
     document.getElementById(layer).style.display = 'none'
